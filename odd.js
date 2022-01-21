c25b0f5ddcaa2f73ecf2633b3869da4f00a2c980
DeebikaSubramanian <97935454+DeebikaSubramanian@users.noreply.github.com>
2022-01-21 15:21:53 +0300

    Add files via upload

---
+++ odd.js
@@ -0,0 +1,10 @@
+//console.log("odd")
+let x=function ()
+{
+    let a=[1,10,15,20,25]
+    for(i=0;i<a.length;i++)
+    if(a[i]%2!=0)
+console.log(a[i])
+}
+console.log("odd numbers using anonymous funct")
+x()
