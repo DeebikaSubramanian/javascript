14dcf728cfce9a4ba1ad2650233bf8d9beccc971
DeebikaSubramanian <97935454+DeebikaSubramanian@users.noreply.github.com>
2022-01-22 20:42:07 +0300

    Add files via upload

---
+++ duplicate.js
@@ -0,0 +1,18 @@
+(()=>{
+    var a=['a','b','d','a','b']   
+let newa=[...new Set(a)]
+console.log(newa)
+})()
+var c=function ()
+{
+    var a=['a','b','d','a','b']   
+let newa=[...new Set(a)]
+console.log(newa)
+}
+var d=()=>
+{var a=['a','b','d','a','b']   
+let newa=[...new Set(a)]
+console.log(newa)
+}
+c()
+d()
